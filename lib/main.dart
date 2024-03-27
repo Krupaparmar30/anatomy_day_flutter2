9e84203953edb7124148221b79fd787bb394c97e
krupa parmar <149374671+Krupaparmar30@users.noreply.github.com>
2024-03-27 15:24:54 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main()
+{
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter New App' ,
-
-      theme: ThemeData(
-
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          leading: Icon(Icons.menu,color: Colors.white,),
+          title: const Text('Flutter App',style: TextStyle(fontWeight: FontWeight.w500,color: Colors.white),),
+        ),
+        body: const Center(
+          child: Text('        Red & White Group of Institutes\nOne Step in Changing Education Chain.....',style: TextStyle(fontWeight: FontWeight.w700,color: Colors.red,fontSize: 16),),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter App'),
-
-
-
-
-
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-
-
-        leading:Icon(Icons.menu,color: Colors.white,),
-
-        backgroundColor:Colors.red,
-
-        title: Text(widget.title),
-      ),
-      body:const Center(
-
-
-        child: Text('        Red & White Group of Institutes \n One Step in Changing Education Chain...',style: TextStyle(fontSize: 16,color: Colors.red,fontWeight: FontWeight.bold),),
-      ),
-
-    );
-  }
+    ),
+  );
 }
